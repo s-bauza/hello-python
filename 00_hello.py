01776e7a4805f994fbeeb0d74825cd7d3c211584
s-bauza <s.bauza@alumnos.upm.es>
2024-04-25 18:30:09 +0200

    00_hello finish and 01 in progress

---
+++ 00_hello.py
@@ -0,0 +1,18 @@
+#Hello World!
+print('Hello World!')
+"""
+This is
+a multi-line
+comment
+"""
+'''
+This is 
+a multi-line
+comment
+'''
+#Check the data type
+print(type("Hello World!")) #<class 'str'>
+print(type(5)) #<class 'int'>
+print(type(5.0)) #<class 'float'>
+print(type(1j)) #<class 'complex'>
+print(type(True)) #<class 'bool'>
---
+++ 01_variables.py
@@ -0,0 +1,17 @@
+# Variables
+
+my_string_varaible = "My String variable"
+print(my_string_varaible)
+
+my_int_variable = 5
+print(my_int_variable)
+
+my_int_to_str_variable = str(my_int_variable)
+print(my_int_to_str_variable)
+print(type(my_int_to_str_variable))#<class 'str'>
+
+my_bool_variable = True
+print(my_bool_variable)
+
+print(my_string_varaible,my_int_variable,my_bool_variable)
+
